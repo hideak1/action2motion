82298b850c9ca3af8157d666563554719d1192b8
Hongxuan Liu <fengjianche@pinduoduo.com>
2022-11-11 15:07:59 -0700

    test

--- train_vq_codebook.py
+++ train_vq_codebook.py
@@ -118,8 +118,8 @@ if __name__ == '__main__':
     # train_split_file = pjoin(opt.data_root, 'train.txt')
     # val_split_file = pjoin(opt.data_root, 'val.txt')
 
-    enc_channels = [opt.dim_vq_latent]
-    dec_channels = [opt.dim_vq_latent, input_size]
+    enc_channels = [1024, opt.dim_vq_latent]
+    dec_channels = [opt.dim_vq_latent, 1024, input_size]
 
     # vq_encoder = VQEncoderV2(dim_pose-4, opt.dim_vq_enc_hidden, opt.dim_vq_latent
 
