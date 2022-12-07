afbc5fd1b12079cd2992c907ff3de98e32c4fb3c
wendy <wendywuchina2013.5@gmail.com>
2022-12-07 14:03:22 -0700

    update

---
+++ motion_loaders/dataset_motion_loader.py
@@ -0,0 +1,31 @@
+from data.dataset import Text2MotionDatasetV2, TestActionTokenDataset
+from motion_loaders.motion_loader import collate_fn
+from utils.word_vectorizer import WordVectorizer
+import numpy as np
+from os.path import join as pjoin
+from torch.utils.data import DataLoader
+from utils.get_opt import get_opt
+
+def get_dataset_motion_loader(opt_path, batch_size, device):
+    opt = get_opt(opt_path, device)
+
+    # Configurations of T2M dataset and KIT dataset is almost the same
+    if opt.dataset_name == 'humanact12' or opt.dataset_name == 'mocap':
+        print('Loading dataset %s ...' % opt.dataset_name)
+
+        mean = np.load(pjoin(opt.meta_dir, 'mean.npy'))
+        std = np.load(pjoin(opt.meta_dir, 'std.npy'))
+
+        w_vectorizer = WordVectorizer('./glove', 'our_vab')
+        split_file = pjoin(opt.data_root, 'test.txt')
+        dataset = Text2MotionDatasetV2(opt, mean, std, split_file, w_vectorizer)
+        dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=4, drop_last=True,
+                                collate_fn=collate_fn, shuffle=True)
+
+        #dataset = TestActionTokenDataset(data, opt, w_vectorizer)
+        #data_loader = DataLoader(dataset, batch_size=opt.batch_size,num_workers=0, shuffle=True, pin_memory=False)
+    else:
+        raise KeyError('Dataset not Recognized !!')
+
+    print('Ground Truth Dataset Loading Completed!!!')
+    return dataloader, dataset
--- visualize_motion_tokens.py
+++ visualize_motion_tokens.py
@@ -139,4 +139,3 @@ if __name__ == '__main__':
             gen_motion = vq_decoder(vq_latent)
 
             plot(gen_motion.cpu().numpy(), str(i))
-
